8c0133b5ec5d7b2e56e84116b46fb3fc34c36b4d
Denis Teslyuk <progamer18@yandex.ru>
2025-06-12 17:55:52 +0300

    Add cart add item function

--- denisshopsite/cart/urls.py
+++ denisshopsite/cart/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
+
+from cart import views
+
 app_name = 'cart'
 
 urlpatterns =[
-
+    path('add-item/<slug:slug>/', views.add_item, name='add_item')
 ]
--- denisshopsite/cart/views.py
+++ denisshopsite/cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import render, redirect
+
+from cart.models import Cart
+from denisshop.models import Game
+
 
 # Create your views here.
+def add_item(request, slug):
+    try:
+        game = Game.objects.get(slug=slug)
+    except:
+        return redirect(request.META.get('HTTP_REFERER'))
+    try:
+        item = Cart.objects.get(game=game, user=request.user)
+    except:
+        Cart.objects.create(game=game, user=request.user)
+        return redirect(request.META.get('HTTP_REFERER'))
+    item.amount = F('amount') + 1
+    item.save()
+    return redirect(request.META.get('HTTP_REFERER'))
