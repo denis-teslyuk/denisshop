09a3b5345085d2433279fada73c863d9d5913f97
Denis Teslyuk <progamer18@yandex.ru>
2025-06-19 15:07:34 +0300

    Alter Cart model collumn type

--- denisshopsite/cart/models.py
+++ denisshopsite/cart/models.py
@@ -7,6 +7,6 @@ from denisshop.models import Game
 # Create your models here.
 class Cart(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='cart',verbose_name='Игры')
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE,
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
                                 related_name='cart', verbose_name='Пользователь')
     amount = models.PositiveIntegerField(verbose_name='Количество', default=1)
