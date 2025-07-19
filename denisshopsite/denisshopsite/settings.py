d6f5de92a72fed79657ef1e2fc7c98934c01334d
Denis Teslyuk <progamer18@yandex.ru>
2025-07-19 16:47:10 +0300

    Alter database to postgres

--- denisshopsite/denisshopsite/settings.py
+++ denisshopsite/denisshopsite/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'denisshopsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': 'denisshop_db',
+        'PASSWORD':1234,
+        'USER':'denisshopsite',
+        'PORT':5432,
+        'HOST':'localhost'
     }
 }
 
